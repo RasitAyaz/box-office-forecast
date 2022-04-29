59b4548adff3594418716ea53a7d7f4342ccceed
Yunus Yıldırım <ynsyldrm6@gmail.com>
2022-04-29 10:18:40 +0300

    Test ANN for different datasets

--- server/src/artificial_neural_network.py
+++ server/src/artificial_neural_network.py
@@ -1,100 +1,85 @@
+import warnings
 import numpy as np
 import pandas as pd
+from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
 
 def relu(z):
-    a = np.maximum(0, z)
+    a = np.maximum(0,z)
     return a
-
 
 def initialize_params(layer_sizes):
     params = {}
     for i in range(1, len(layer_sizes)):
-        params['W' + str(i)] = np.random.randn(layer_sizes[i],
-                                               layer_sizes[i-1])
-        params['B' + str(i)] = np.random.randn(layer_sizes[i], 1)
-    print(params)
+        params['W' + str(i)] = np.random.randn(layer_sizes[i], layer_sizes[i-1])*0.01
+        params['B' + str(i)] = np.random.randn(layer_sizes[i],1)*0.01
     return params
-
 
 def forward_propagation(X_train, params):
     layers = len(params)//2
     values = {}
     for i in range(1, layers+1):
-        if i == 1:
-            values['Z' + str(i)] = np.dot(params['W' + str(i)],
-                                          X_train) + params['B' + str(i)]
+        if i==1:
+            values['Z' + str(i)] = np.dot(params['W' + str(i)], X_train) + params['B' + str(i)]
             values['A' + str(i)] = relu(values['Z' + str(i)])
         else:
-            values['Z' + str(i)] = np.dot(params['W' + str(i)],
-                                          values['A' + str(i-1)]) + params['B' + str(i)]
-            if i == layers:
+            values['Z' + str(i)] = np.dot(params['W' + str(i)], values['A' + str(i-1)]) + params['B' + str(i)]
+            if i==layers:
                 values['A' + str(i)] = values['Z' + str(i)]
             else:
                 values['A' + str(i)] = relu(values['Z' + str(i)])
     return values
 
-
 def compute_cost(values, Y_train):
     layers = len(values)//2
     Y_pred = values['A' + str(layers)]
-    cost = (1/(2*len(Y_train))) * np.sum(np.square(Y_pred - Y_train))
+    cost = 1/(2*len(Y_train)) * np.sum(np.square(Y_pred - Y_train))
     return cost
-
 
 def backward_propagation(params, values, X_train, Y_train):
     layers = len(params)//2
     m = len(Y_train)
     grads = {}
-    for i in range(layers, 0, -1):
-        if i == layers:
-            dA = 1/m * (values['A' + str(i)] + Y_train)
+    for i in range(layers,0,-1):
+        if i==layers:
+            dA = 1/m * (values['A' + str(i)] - Y_train)
             dZ = dA
         else:
             dA = np.dot(params['W' + str(i+1)].T, dZ)
-            dZ = np.multiply(dA, np.where(values['A' + str(i)] >= 0, 1, 0))
-        if i == 1:
-            grads['W' + str(i)] = 1/m * np.dot(dZ, X_train.T)
+            dZ = np.multiply(dA, np.where(values['A' + str(i)]>=0, 1, 0))
+        if i==1:
+            grads['W' + str(i)] = 1/m * np.dot(dZ, X_train.T    )
             grads['B' + str(i)] = 1/m * np.sum(dZ, axis=1, keepdims=True)
         else:
-            grads['W' + str(i)] = 1/m * np.dot(dZ, values['A' + str(i-1)].T)
+            grads['W' + str(i)] = 1/m * np.dot(dZ,values['A' + str(i-1)].T)
             grads['B' + str(i)] = 1/m * np.sum(dZ, axis=1, keepdims=True)
     return grads
-
 
 def update_params(params, grads, learning_rate):
     layers = len(params)//2
     params_updated = {}
-    for i in range(1, layers+1):
-        params_updated['W' + str(i)] = params['W' + str(i)] - \
-            learning_rate * grads['W' + str(i)]
-        params_updated['B' + str(i)] = params['B' + str(i)] - \
-            learning_rate * grads['B' + str(i)]
+    for i in range(1,layers+1):
+        params_updated['W' + str(i)] = params['W' + str(i)] - learning_rate * grads['W' + str(i)]
+        params_updated['B' + str(i)] = params['B' + str(i)] - learning_rate * grads['B' + str(i)]
     return params_updated
-
 
 def model(X_train, Y_train, layer_sizes, num_iters, learning_rate):
     params = initialize_params(layer_sizes)
     for i in range(num_iters):
         values = forward_propagation(X_train.T, params)
         cost = compute_cost(values, Y_train.T)
-        grads = backward_propagation(params, values, X_train.T, Y_train.T)
-        #params = update_params(params, grads, learning_rate)
-        print('Cost at iteration ' + str(i+1) + ' = ' + str(cost) + '\n')
+        grads = backward_propagation(params, values,X_train.T, Y_train.T)
+        params = update_params(params, grads, learning_rate)
     return params
-
 
 def compute_accuracy(X_train, X_test, Y_train, Y_test, params):
     values_train = forward_propagation(X_train.T, params)
     values_test = forward_propagation(X_test.T, params)
-    train_acc = np.sqrt(mean_squared_error(
-        Y_train, values_train['A' + str(len(layer_sizes)-1)].T))
-    test_acc = np.sqrt(mean_squared_error(
-        Y_test, values_test['A' + str(len(layer_sizes)-1)].T))
+    train_acc = np.sqrt(mean_squared_error(Y_train, values_train['A' + str(len(layer_sizes)-1)].T))
+    test_acc = np.sqrt(mean_squared_error(Y_test, values_test['A' + str(len(layer_sizes)-1)].T))
     return train_acc, test_acc
-
 
 def predict(X, params):
     values = forward_propagation(X.T, params)
@@ -102,15 +87,17 @@ def predict(X, params):
     return predictions
 
 
-dataset_path = 'server/data/dataset.csv'
+dataset_path = 'server/dataset.csv'
 data = pd.read_csv(dataset_path)
-
 X = data.iloc[:, 0:-1].values
 Y = data.iloc[:, -1].values
 
-layer_sizes = [4, 2, 2, 1]
-num_iters = 100
-learning_rate = 0.005
+# data = load_boston()
+# X,Y = data['data'],data['target']
+
+layer_sizes = [5, 4, 4, 1]
+num_iters = 1000
+learning_rate = 0.03
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20)
 
