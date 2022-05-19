93a596fb7c8e1d652c9d70ab4ec743766338d34e
RasitAyaz <rasitayaz1358@gmail.com>
2022-05-19 16:08:15 +0300

    Add overall scores

--- server/src/test.py
+++ server/src/test.py
@@ -1,10 +1,10 @@
-from genericpath import isfile
 import os
+
 import pandas as pd
+from genericpath import isfile
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
-
 
 current_path = os.path.dirname(__file__)
 dataset_path = f'{current_path}/../dataset.csv'
@@ -17,7 +17,9 @@ df = data.copy()
 X = df.drop("revenue", axis=1)
 y = df["revenue"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state= 100)
+print('---------------------------')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state= 100)
 
 lr = LinearRegression().fit(X_test, y_test)
 
@@ -27,13 +29,23 @@ print(f'LR train score: {score}')
 score = lr.score(X_test, y_test)
 print(f'LR test score: {score}')
 
+score = lr.score(X, y)
+print(f'LR overall score: {score}')
+
+print('---------------------------')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state= 100)
+
 ann = MLPRegressor(max_iter=1000).fit(X_train, y_train)
 
 score = ann.score(X_train, y_train)
-print(f'ANN score: {score}')
+print(f'ANN train score: {score}')
 
 score = ann.score(X_test, y_test)
-print(f'ANN score: {score}')
+print(f'ANN test score: {score}')
+
+score = ann.score(X, y)
+print(f'ANN overall score: {score}')
 
 
 
