6584391d2a3c72175fe402e8d6d155193f0d215b
Yunus Yıldırım <ynsyldrm6@gmail.com>
2022-05-28 12:24:08 +0300

    Store model to disk

--- server/src/test.py
+++ server/src/test.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 import pandas as pd
 from genericpath import isfile
@@ -25,12 +26,19 @@ print('---------------------------')
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 lr = LinearRegression().fit(X_test, y_test)
 
+# Store linear regression the model to disk
+filename = '{current_path}/../models/linear_regression.sav'.format(current_path=current_path)
+pickle.dump(lr, open(filename, 'wb'))
+
+# linear regression model r^2 score on test data
 score = lr.score(X_train, y_train)
 print(f'LR train score: {score}')
 
+# linear regression model r^2 score on test data
 score = lr.score(X_test, y_test)
 print(f'LR test score: {score}')
 
+# linear regression model r^2 score on overall data
 score = lr.score(X, y)
 print(f'LR overall score: {score}')
 
@@ -39,12 +47,19 @@ print('---------------------------')
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 ann = MLPRegressor(max_iter=1000,hidden_layer_sizes=10,learning_rate_init=0.05).fit(X_train, y_train)
 
+# Store artificial neural network the model to disk
+filename = '{current_path}/../models/artificial_neural_network.sav'.format(current_path=current_path)
+pickle.dump(lr, open(filename, 'wb'))
+
+# artificial neural network model r^2 score on train data
 score = ann.score(X_train, y_train)
 print(f'ANN train score: {score}')
 
+# artificial neural network model r^2 score on test data
 score = ann.score(X_test, y_test)
 print(f'ANN test score: {score}')
 
+# artificial neural network model r^2 score on overall data
 score = ann.score(X, y)
 print(f'ANN overall score: {score}')
 
@@ -52,14 +67,20 @@ print('---------------------------')
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 svr = make_pipeline(SVR(kernel = "linear", C=1.0, epsilon=0.2)).fit(X_train, y_train)
-svr2 = make_pipeline(SVR(kernel = "rbf", C=1.0, epsilon=0.2)).fit(X_train, y_train)
 
+# store support vector regression the model to disk
+filename = '{current_path}/../models/support_vector_regression.sav'.format(current_path=current_path)
+pickle.dump(lr, open(filename, 'wb'))
+
+# svr r^2 score for training data
 score = svr.score(X_train, y_train)
 print(f'SVR with linear kernel train score: {score}')
 
+# svr r^2 score for test data
 score = svr.score(X_test, y_test)
 print(f'SVR with linear kernel test score: {score}')
 
+# svr r^2 score for overall data
 score = svr.score(X, y)
 print(f'SVR with linear kernel overall score: {score}')
 
