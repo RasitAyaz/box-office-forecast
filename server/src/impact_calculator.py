4c6b4e0b943d5753778b35e595d8d54806dfc71c
RasitAyaz <rasitayaz1358@gmail.com>
2022-05-30 23:08:51 +0300

    Next generation xgb calculation

--- server/src/impact_calculator.py
+++ server/src/impact_calculator.py
@@ -140,16 +140,31 @@ def display_impacts(feature, sorted_importances):
     plt.show()
 
 
-def store_impacts_to_csv(feature, importance_data):
-    data = pd.DataFrame(importance_data)
+def store_impacts_to_csv(feature, impacts: dict):
+    data_items = []
+    for id, item in impacts.items():
+        data_item = {
+            'id': id,
+            'name': item['name'],
+        }
+
+        for year in range(1990, 1995):
+            if year not in item['impacts']:
+                data_item[f'{year}'] = 0
+            else:
+                data_item[f'{year}'] = item['impacts'][year]
+        
+        data_items.append(data_item)
+
+    data = pd.DataFrame(data_items)
     path = f'{current_path}/../data/impacts/{feature}.csv'
     data.to_csv(path, index=False)
 
 
-def filter_by_impact_and_store(feature, list_feature, counts: dict, min_importance):
+
+def calculate_importances(year, feature, list_feature, counts: dict, lower_bound=0):
     data_items = []
 
-    print('before scanning movies')
     for movie in all_movies:
         row = {}
 
@@ -179,145 +194,112 @@ def filter_by_impact_and_store(feature, list_feature, counts: dict, min_importan
         
         row['revenue'] = movie['revenue']
         data_items.append(row)
-    print('after scanning movies')
 
     data: pd.DataFrame
-    print('before creating df')
     data = pd.DataFrame(data_items)
-    print('after creating df')
     X = data.iloc[:, 0:-1]
     X_headers = data.columns[0:-1].values
     y = data.iloc[:, -1]
 
     regressor = XGBRegressor()
-    print('before xgb')
     regressor.fit(X, y)
-    print('after xgb')
 
     importances = {}
 
     count = 0
     for feature_importance in regressor.feature_importances_:
-        if feature_importance > min_importance:
+        if feature_importance > lower_bound:
             feature_id = X_headers[count]
             importances[feature_id] = feature_importance
         count += 1
 
-    importance_data_items = []
     
     for id, item in counts.items():
         if id not in importances:
             continue
-        
-        importance_data_items.append({
-            'id': id,
-            'name': get_name(item),
-            'importance': importances[id]
-        })
 
-    importance_data_items = sorted(importance_data_items, key=lambda item: item['importance'], reverse=True)
-
-    importance_data: pd.DataFrame
-    importance_data = pd.DataFrame(importance_data_items)
-
-    # Get top 40 features
-    # sorted_importances = sorted_importances[0:40]
-
-    # display_impacts(feature, sorted_importances)
+        if feature not in impact_json:
+            impact_json[feature] = {}
+        if id not in impact_json[feature]:
+            impact_json[feature][id] = {
+                'name': item['name'],
+                'impacts': {year: importances[id]}
+            }
+        else:
+            item_json = impact_json[feature][id]
+            item_json['impacts'][year] = importances[id]
     
-    print(f'{feature}: {len(importance_data)} / {len(counts)} (importance >= {min_importance})')
+    print(f'{year} {feature}: {len(importances)} / {len(counts)} (importance > {lower_bound})')
     print('----------------------------------------------------')
 
-    store_impacts_to_csv(feature, importance_data)
+impact_json = {}
 
 
-month_counts = {}
-original_language_counts = {}
+for max_year in range(1991, 1995):
+    month_counts = {}
+    original_language_counts = {}
 
-genre_counts = {}
-company_counts = {}
-country_counts = {}
-language_counts = {}
-cast_counts = {}
-keyword_counts = {}
+    genre_counts = {}
+    company_counts = {}
+    country_counts = {}
+    language_counts = {}
+    cast_counts = {}
+    keyword_counts = {}
 
-directing_counts = {}
-writing_counts = {}
-production_counts = {}
-editing_counts = {}
-camera_counts = {}
-art_counts = {}
-sound_counts = {}
-costume_counts = {}
+    directing_counts = {}
+    writing_counts = {}
+    production_counts = {}
+    editing_counts = {}
+    camera_counts = {}
+    art_counts = {}
+    sound_counts = {}
+    costume_counts = {}
 
-all_movies = []
+    all_movies = []
 
-for year in range(1990, 2020):
-    path = f'{current_path}/../data/years/{year}.json'
-    if isfile(path):
-        movies_of_year = json.load(open(path))
-        for id, movie in movies_of_year.items():
-            # Skip invalid movies
-            if len(movie['genres']) == 0:
-                continue
-            
-            all_movies.append(movie)
-            count_features(genre_counts, movie['genres'])
-            count_features(company_counts, movie['production_companies'])
-            count_features(country_counts, movie['production_countries'])
-            count_features(language_counts, movie['spoken_languages'])
-            count_features(cast_counts, movie['cast'])
-            count_features(keyword_counts, movie['keywords'])
-            count_crew_members(movie['crew'])
-            count_month(get_month(movie['release_date']))
-            count_original_language(movie['original_language'])
-    else:
-        print(f'{path} could not be found.')
-        exit()
+    for year in range(1990, max_year):
+        path = f'{current_path}/../data/years/{year}.json'
+        if isfile(path):
+            movies_of_year = json.load(open(path))
+            for id, movie in movies_of_year.items():
+                # Skip invalid movies
+                if len(movie['genres']) == 0:
+                    continue
+                
+                all_movies.append(movie)
+                count_features(genre_counts, movie['genres'])
+                count_features(company_counts, movie['production_companies'])
+                count_features(country_counts, movie['production_countries'])
+                count_features(language_counts, movie['spoken_languages'])
+                count_features(cast_counts, movie['cast'])
+                count_features(keyword_counts, movie['keywords'])
+                count_crew_members(movie['crew'])
+                count_month(get_month(movie['release_date']))
+                count_original_language(movie['original_language'])
+        else:
+            print(f'{path} could not be found.')
+            exit()
+    
+    calculate_importances(max_year, 'months', None, month_counts)
+    calculate_importances(max_year, 'original_languages', None, original_language_counts)
+
+    calculate_importances(max_year, 'genres', 'genres', genre_counts)
+    calculate_importances(max_year, 'companies', 'production_companies', company_counts)
+    calculate_importances(max_year, 'countries', 'production_countries', country_counts)
+    calculate_importances(max_year, 'languages', 'spoken_languages', language_counts)
+    calculate_importances(max_year, 'keywords', 'keywords', keyword_counts)
+    calculate_importances(max_year, 'cast', 'cast', cast_counts)
+
+    calculate_importances(max_year, 'directing', 'crew', directing_counts) 
+    calculate_importances(max_year, 'writing', 'crew', writing_counts)
+    calculate_importances(max_year, 'production', 'crew', production_counts)
+    calculate_importances(max_year, 'editing', 'crew', editing_counts)
+    calculate_importances(max_year, 'camera', 'crew', camera_counts)
+    calculate_importances(max_year, 'art', 'crew', art_counts)
+    calculate_importances(max_year, 'sound', 'crew', sound_counts)
+    calculate_importances(max_year, 'costume', 'crew', costume_counts)
 
 
-print('\n\nCOUNT FILTERS')
-print('----------------------------------------------------')
+for feature, impacts in impact_json.items():
+    store_impacts_to_csv(feature, impacts)
 
-# filter_by_count('months', month_counts, min_count=0)
-# filter_by_count('original_languages', original_language_counts, min_count=3)
-# 
-# filter_by_count('genres', genre_counts, min_count=0)
-# filter_by_count('companies', company_counts, min_count=5)
-# filter_by_count('countries', country_counts, min_count=5)
-# filter_by_count('languages', language_counts, min_count=20)
-# filter_by_count('keywords', keyword_counts, min_count=0)
-filter_by_count('cast', cast_counts, min_count=0)
-# 
-# filter_by_count('directing', directing_counts, min_count=2)
-# filter_by_count('writing', writing_counts, min_count=2)
-# filter_by_count('production', production_counts, min_count=2)
-# filter_by_count('editing', editing_counts, min_count=2)
-# filter_by_count('camera', camera_counts, min_count=2)
-# filter_by_count('art', art_counts, min_count=2)
-# filter_by_count('sound', sound_counts, min_count=2)
-# filter_by_count('costume', costume_counts, min_count=2)
-
-print('\n\nIMPORTANCE FILTERS')
-print('----------------------------------------------------')
-
-# filter_by_impact_and_store('months', None, month_counts, min_importance=0.01)
-# filter_by_impact_and_store('original_languages', None, original_language_counts, min_importance=1e-4)
-# 
-# filter_by_impact_and_store('genres', 'genres', genre_counts, min_importance=0.01)
-# filter_by_impact_and_store('companies', 'production_companies', company_counts, min_importance=1e-5)
-# filter_by_impact_and_store('countries', 'production_countries', country_counts, min_importance=1e-03)
-# filter_by_impact_and_store('languages', 'spoken_languages', language_counts, min_importance=1e-2)
-# filter_by_impact_and_store('keywords', 'keywords', keyword_counts, min_importance=1e-5)
-filter_by_impact_and_store('cast', 'cast', cast_counts, min_importance=1e-5)
-# 
-# filter_by_impact_and_store('directing', 'crew', directing_counts, min_importance=1e-6) 
-# filter_by_impact_and_store('writing', 'crew', writing_counts, min_importance=1e-6)
-# filter_by_impact_and_store('production', 'crew', production_counts, min_importance=1e-6)
-# filter_by_impact_and_store('editing', 'crew', editing_counts, min_importance=1e-6)
-# filter_by_impact_and_store('camera', 'crew', camera_counts, min_importance=1e-6)
-# filter_by_impact_and_store('art', 'crew', art_counts, min_importance=1e-6)
-# filter_by_impact_and_store('sound', 'crew', sound_counts, min_importance=1e-6)
-# filter_by_impact_and_store('costume', 'crew', costume_counts, min_importance=1e-6)
-
-print(f'\nTotal movies: {len(all_movies)}')
